7662aad95497e72d35ae4cdce0d7f134f891d86c
seunghee17 <victoria9981@swu.ac.kr>
2024-06-06 17:13:36 +0900

    fix: 글쓰기 화면 배경색상 변경

--- app/src/main/java/com/daepiro/numberoneproject/presentation/view/community/TownCommentListAdapter.kt
+++ app/src/main/java/com/daepiro/numberoneproject/presentation/view/community/TownCommentListAdapter.kt
@@ -1,6 +1,5 @@
 package com.daepiro.numberoneproject.presentation.view.community
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,6 @@ import com.daepiro.numberoneproject.R
 import com.daepiro.numberoneproject.data.model.Content
 
 class TownCommentListAdapter(
-    //private var items: List<Content> = listOf(),
     private val listener: onItemClickListener,
     private val getTimeDifference: (String) -> String
 ): PagingDataAdapter<Content, TownCommentListAdapter.ViewHolder>(DIFF_CALLBACK) {
@@ -40,7 +38,6 @@ class TownCommentListAdapter(
         val replynum:TextView = itemView.findViewById(R.id.reply_num)
 
         fun bind(item: Content, listener: onItemClickListener, getTimeDifference: (String) -> String){
-            //val item = items[position]
             title.text = item.title
             tag.text = when(item.tag.toString()){
                 "LIFE" -> "일상"
@@ -67,60 +64,13 @@ class TownCommentListAdapter(
 
 
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_community_townlist,parent,false)
         return ViewHolder(view)
     }
 
-//    override fun getItemCount(): Int {
-//        return items.size
-//    }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//        if(position < items.size){
-//            val item = items[position]
-//            holder.title.text = item.title
-//            holder.tag.text = when(item.tag.toString()){
-//                "LIFE" -> "일상"
-//                "TRAFFIC" -> "교통"
-//                "SAFETY" -> "치안"
-//                 "NONE" -> "기타"
-//                else-> "전체"
-//            }
-//            holder.content.text = item.content
-//            //시간 처리
-//            val time = getTimeDifference(item.createdAt)
-//            holder.writerInfo.text = " ${item.address} ∙ ${item.ownerNickName} ∙ ${time}"
-//
-//            Glide.with(holder.itemView.context)
-//                .load(item.thumbNailImageUrl)
-//                .into(holder.image)
-//
-//            holder.likenum.text = item.articleLikeCount.toString()
-//            holder.replynum.text = item.commentCount.toString()
-//            holder.itemView.setOnClickListener{
-//                listener.onItemClick(item.id)
-//            }
-//
-//
-//        }
         val item = getItem(position) ?: return
         holder.bind(item, listener, getTimeDifference)
-
     }
-//    fun updateList(newData:List<Content>){
-//        Log.d("AdapterDebug", "updateList called with data: ${newData.size}")
-//        val startIndox = items.size
-//        items = items + newData
-//        //notifyItemRangeChanged(startIndox,newData.size)
-//        notifyDataSetChanged()
-//        Log.d("AdapterDebutg", "update list")
-//    }
-//    fun clearData(){
-//        items = listOf()
-//        notifyDataSetChanged()
-//        Log.d("AdapterDebutg", "Data cleared")
-//    }
-
 }
